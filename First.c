f75278fe4d4f77166dfad3ec72abe1d6e59d5a21
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-30 15:44:12 +0530

    Update First.c

--- First.c
+++ First.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main() 
 {
+    int number = 23;
+    char stri = 's';
+    // string name = "sonu"; string is not defined
+    // boolean name = true; Boolean is not defined
     printf("hello World\n");
     printf("hello World");
     return 0;
