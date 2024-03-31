e04353eacbcd3135d9ef45ba03fc2a41b8a4a087
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-31 17:15:19 +0530

    update

--- First.c
+++ First.c
@@ -3,9 +3,13 @@ int main()
 {
     int number = 23;
     char stri = 's';
+
     // string name = "sonu"; string is not defined
     // boolean name = true; Boolean is not defined
-    printf("hello World\n");
-    printf("hello World");
+    printf("hello World\n"); //back slash n (\n) is defined next line ya break line
+    printf("hello World\n"); //back slash n (\n) is defined next line ya break line
+    printf("variable number is %d" , number);
+    // printf("pi value is %d" , pi);
+    printf("charecter is %c", stri);
     return 0;
 }
