c763e9bab3882ffccf3b34a11574e148773dd054
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-31 17:28:33 +0530

    new file

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main(){
+    int a;
+    int b;
+    printf("enter  first number:- ");
+    scanf("%d", &a);
+    printf("enter second number :- ");
+    scanf("%d", &b);
+    int sum = a+b;
+    printf("add all number equal to : %d", sum);
+    return 0;
+ }
