726349d7d086f91ffadbba009007ce2f3c1208c6
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-07 18:12:32 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -4,10 +4,10 @@ int main(){
     printf("enter a number");  //153
     scanf("%d",&num);
     int str = num;  //str = 153
-    while(num>0){
+    while(num>0){ 
         a = num%10;  //a= 153/10 = 3   //a= 15%
         arm = (a*a*a)+arm ;  // arm = (3*3*3)+0 = 27
-        num = num/10;
+        num = num/10;  //num = 15 // num = 1
     }
     if(str == arm){
         printf("this number is Armstrong Number");
