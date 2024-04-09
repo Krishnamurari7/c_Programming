8d2fc5f31255cbc8d3d6951466f0f5648b37efd4
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-09 21:48:07 +0530

    second method

--- postive_check.c
+++ postive_check.c
@@ -3,9 +3,12 @@ int main(){
     int number;
     printf("Enter a number : -");
     scanf("%d", &number);
-    if(number > 0){
-        printf("this number is Positive");
-    }
-    // printf("This number is Not positive");
+    // if(number > 0){
+    //     printf("this number is Positive");
+    // }
+    // // if(number <= 0)//if condition use
+    // else printf("This number is Not positive"); //if else use
+
+    (number> 0)? printf("this number is Positive") : printf("This number is Not positive"); //ternary operator used
     return 0;
 }
