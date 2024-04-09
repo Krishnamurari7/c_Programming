6f4b07ca64b52ceac03a9889f8dc1a6bc7060be6
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-09 18:45:00 +0530

    second method

--- swap_value.c
+++ swap_value.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
 
-int main(){ 
+// int main(){ 
+//     int a = 10;
+//     int b = 20;
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     printf("a=%d b=%d", a, b);
+//     return 0;
+// }
+
+
+
+
+//without  third variable
+int main(){
     int a = 10;
     int b = 20;
-    int temp = a;
-    a = b;
-    b = temp;
-    printf("a=%d b=%d", a, b);
-    return 0;
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    printf("a=%d b=%d",a , b);
 }
