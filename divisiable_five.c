743d65cbc11ace59166509c708432a44fd7eb8d0
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-10 17:52:14 +0530

    new program

---
+++ divisiable_five.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter any Number:-");
+    scanf("%d",&num);
+    if(num%5==0){
+        printf("This number is divisiable by 5");
+    }else{
+        printf("this number is not divisiable by 5");
+    }
+}
