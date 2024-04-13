48e1f7143d76ae11a206d3820af457676e22f6cb
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-13 15:33:41 +0530

    update

--- armstrong.c
+++ armstrong.c
@@ -4,9 +4,17 @@ int main(){
     printf("enter a number");  //153
     scanf("%d",&num);
     int str = num;  //str = 153
+    // int count = 0;
+    // while(num>0){
+    //     int check = num/10;
+    //      count++;
+    // }
+
+
+
     while(num>0){ 
         a = num%10;  //a= 153/10 = 3   //a= 15%
-        arm = (a*a*a)+arm ;  // arm = (3*3*3)+0 = 27
+        arm = a*a+arm ;  // arm = (3*3*3)+0 = 27
         num = num/10;  //num = 15 // num = 1
     }
     if(str == arm){
